9bbdea0487fd9533ccef7e0c3404beee787fef6e
vinayakhegde1 <73926849+vinayakhegde1@users.noreply.github.com>
2023-01-11 00:12:11 +0530

    Update Home.jsx

--- ott/src/components/Home.jsx
+++ ott/src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+
 const Home = () => {
-    
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1 className='text-white font-sans font-bold text-5xl flex items-center justify-center py-10'>Home page</h1>
+      <div className='text-white font-sans font-bold text-5xl flex items-center justify-center py-6'><span>Site Underwork</span></div>
     </div>
   )
 }
 
 export default Home
+
